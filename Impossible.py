3834428cef56210d77af77860b073033a0650032
SantiagoGV29 <73509100+SantiagoGV29@users.noreply.github.com>
2023-08-03 17:54:32 -0500

    push

--- Impossible.py
+++ Impossible.py
@@ -27,3 +27,41 @@ def Create_Combinations(nums, n):
     combinations = []
     generate_combinations(0, [])
     return combinations
+
+#Optimized
+
+class Solution:
+    def minimumDifference(self, nums):
+        def get_combinations(arr, k):
+            def generate_combinations(start, current_comb):
+                if len(current_comb) == k:
+                    res.append(list(current_comb))
+                    return
+                for i in range(start, len(arr)):
+                    current_comb.append(arr[i])
+                    generate_combinations(i + 1, current_comb)
+                    current_comb.pop()
+            res = []
+            generate_combinations(0, [])
+            return sorted(res,key=sum)
+
+        N = len(nums) // 2
+        ans = abs(sum(nums[:N]) - sum(nums[N:]))
+        total = sum(nums)
+        half = total // 2
+
+        for k in range(1, N):
+            left_combinations = get_combinations(nums[:N], k)
+            right_combinations = get_combinations(nums[N:], N - k)
+            right = [sum(comb) for comb in right_combinations]
+
+            for x in left_combinations:
+                r = half - sum(x)
+                p = bisect.bisect_left(right, r)
+                if 0 <= p < len(right):
+                    left_ans_sum = sum(x) + right[p]
+                    right_ans_sum = total - left_ans_sum
+                    diff = abs(left_ans_sum - right_ans_sum)
+                    ans = min(ans, diff)
+
+        return ans
